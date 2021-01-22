21e679916723320304c2aa2dedb45aad78421c58
Jhons <jhons.albornoz.araya@gmail.com>
2021-01-22 19:38:03 -0300

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -4,12 +4,13 @@ const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
 
 const app = express();
 
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -45,7 +46,7 @@ app.get('/usuario', function (req, res) {
 
 });
   
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
     
     let body = req.body;
 
@@ -76,7 +77,7 @@ app.post('/usuario', function (req, res) {
 
 });
   
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
     let body = _.pick( req.body, ['nombre', 'email', 'img', 'role', 'estado'] );
@@ -99,7 +100,7 @@ app.put('/usuario/:id', function (req, res) {
 
 });
   
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
 
@@ -118,15 +119,6 @@ app.delete('/usuario/:id', function (req, res) {
                 err
             });
         };
-
-        if ( usuarioBorrado.estado === false ){
-            return res.status(400).json({
-                ok: false,
-                err: {
-                    message: 'Usuario no encontrado'
-                }
-            });
-        }
 
         res.json({
             ok: true,
